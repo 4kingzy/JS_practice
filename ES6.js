8a62af13832bf96c7a4c5193b567995a78923357
Mario Vlahoviček <100935597+4kingzy@users.noreply.github.com>
2022-09-21 13:23:32 +0200

    Create ES6.js

---
+++ ES6.js
@@ -0,0 +1,15 @@
+let experience = 200;
+let wizardLevel = false;
+
+if (experience > 90) {
+let wizardLevel= true;
+}
+
+//property of an object can be changed in constant variable with [obj.wizardLvl = true;]
+
+const obj = {
+player: "bobby";
+exp: 100;
+wizardLvl: false;
+}
+
