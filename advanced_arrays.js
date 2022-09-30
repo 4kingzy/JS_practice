00c91e44111cbf1a555dc3ed31415ac76f73b9dc
Mario Vlahoviček <100935597+4kingzy@users.noreply.github.com>
2022-09-30 10:22:16 +0200

    Create advanced_arrays.js

---
+++ advanced_arrays.js
@@ -0,0 +1,17 @@
+//arrays - For Each recuperation
+
+ 
+const double = [];
+const newArray = array.forEach((num) => {
+    double.push(num * 2); 
+})
+
+console.log('for Each', newArray);
+
+//map
+
+const mapArray = array.map((num) => {
+    return num * 2
+});
+
+console.log('map', mapArray);
