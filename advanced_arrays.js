9caec76caea45d5c509fed1e4574442f8e80eab4
Mario Vlahoviček <100935597+4kingzy@users.noreply.github.com>
2022-09-30 10:31:54 +0200

    Update advanced_arrays.js

--- advanced_arrays.js
+++ advanced_arrays.js
@@ -1,8 +1,9 @@
 //arrays - For Each recuperation
+// if you ave a single parameter you can avoid brackets, in this case on "num"
 
  
 const double = [];
-const newArray = array.forEach((num) => {
+const newArray = array.forEach(num => {
     double.push(num * 2); 
 })
 
@@ -10,7 +11,7 @@ console.log('for Each', newArray);
 
 //map
 
-const mapArray = array.map((num) => {
+const mapArray = array.map(num => {
     return num * 2
 });
 
