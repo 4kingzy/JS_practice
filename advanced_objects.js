60b62c9d6c55da4dace173809d03fb0296468f2e
Mario Vlahoviček <100935597+4kingzy@users.noreply.github.com>
2022-10-03 09:20:14 +0200

    Create advanced_objects.js

---
+++ advanced_objects.js
@@ -0,0 +1,34 @@
+//Reference Type - with each object we create a "box" with a designated value in it, and with a case of object2 we are referencing object 1
+
+let object1 = {value:10};
+let object2 = object1;
+let object3 = {value: 10};
+
+//context vs scope - "this" tells us what is inside of a scope hes in
+
+const object4 = {
+    a: function() {
+        console.log(this);
+    }
+}
+
+//instantiation
+class Player {
+    constructor (name,type) {
+        this.name = name;
+        this.type = type;
+    }
+    introduce() {
+        console.log(`Hi I am ${this.name}, I'm a ${this.type}`)
+    }
+}
+class Wizard extends Player {
+    constructor(name,type) {
+        super(name,type)
+    }
+    play() {
+        console.log(`WEEEEEE I'm a ${this.type}`);
+    }
+}
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Dominique', 'Barbarian');
