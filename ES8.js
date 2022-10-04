0cd4ee0fb364e2f8f7b584f1dfa0c62e2b38b842
Mario Vlahoviček <100935597+4kingzy@users.noreply.github.com>
2022-10-04 11:25:53 +0200

    Create ES8.js

---
+++ ES8.js
@@ -0,0 +1,30 @@
+//PadStart and PadEnd --> creates a spase on front or end of string equal to the input number
+
+"Turtle".padStart(19);
+"Turtle".padEnd(19);
+
+//Object Value and Object Entries
+//Object.keys
+let obj = {
+    username0 : "Maja",
+    username1: "Mario",
+    username2: "Mahalec"
+}
+Object.keys(obj).forEach((key,index) => {
+    console.log(key, obj[key]);
+})
+
+//Object.value
+Object.values(obj).forEach(value => {
+    console.log(value);
+})
+
+//Object.entries
+Object.entries(obj).forEach(value => {
+    console.log(value);
+})
+
+//example with a entries
+Object.entries(obj).map(value => {
+    return value[1] + value[0].replace("username", "");
+})
