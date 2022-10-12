5dc4126191223a7b6cefbac5f307fb255db40207
Mario Vlahoviček <100935597+4kingzy@users.noreply.github.com>
2022-10-12 09:12:12 +0200

    Update modules.js

--- modules.js
+++ modules.js
@@ -16,3 +16,13 @@ import {add} from "./add";
 //or
 
 import add from "./add";
+
+
+//Webpack - example of a Webpack
+module.exports = {
+   entry: "./app/main.js",
+   output: {
+      path: "./dist",
+      filename: "bundle.js"
+   }
+}
